4edab4d7a4479b3f1d781b2718dd04d8380badbc
EinHades <kramertom964@gmail.com>
2026-03-10 15:35:09 -0400

    Cleaned up the Script by adding Categories. Added the following app either to install Discord, Raycast, WSL, Epicgames, Quickshare. Added the following apps to remove: Xbox Gamebar

--- Windows.ps1
+++ Windows.ps1
@@ -1,9 +1,10 @@
+#Update System
 winget upgrade --all
 
+#Install Programs
 winget install -e --id Git.Git
 winget install -e --id Ollama.Ollama
 winget install -e --id Zen-Team.Zen-Browser
-winget remove -e --id Microsoft.Edge
 winget install -e --id Autodesk.Fusion360
 winget install -e --id Anthropic.Claude
 winget install -e --id Tailscale.Tailscale
@@ -11,3 +12,14 @@ winget install -e --id Mozilla.Firefox
 winfet install -e --id 9PFXXSHC64H3 --source msstore
 winget install -e --id dotPDN.PaintDotNet
 winget install -e --id 9NKSQGP7F2NH --source msstore
+winget install -e --id Discord.Discord
+winget install -e --id 9P94LH3Q1CP5 --source msstore
+winget install -e --id Microsoft.WSL
+winget install -e --id EpicGames.EpicGamesLauncher
+winget install -e --id Google.QuickShare
+winget install -e --id 
+
+#Remove Bloatware
+winget remove -e --id Microsoft.Edge
+winget remove -e --id Xbox.XboxGameBar
+winget remove -e --id Microsoft.
