ee0386028d89218e5ab3cd3598eec493ea27a573
EinHades <kramertom964@gmail.com>
2026-03-10 15:02:31 -0400

    Added a script for Windows

---
+++ Windows.ps1
@@ -0,0 +1,13 @@
+winget upgrade --all
+
+winget install -e --id Git.Git
+winget install -e --id Ollama.Ollama
+winget install -e --id Zen-Team.Zen-Browser
+winget remove -e --id Microsoft.Edge
+winget install -e --id Autodesk.Fusion360
+winget install -e --id Anthropic.Claude
+winget install -e --id Tailscale.Tailscale
+winget install -e --id Mozilla.Firefox
+winfet install -e --id 9PFXXSHC64H3 --source msstore
+winget install -e --id dotPDN.PaintDotNet
+winget install -e --id 9NKSQGP7F2NH --source msstore
